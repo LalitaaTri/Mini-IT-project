9c33098d11c5fd39c5224751df624b7e11d3c6a4
alimq <alimimanmalik2@gmail.com>
2026-04-17 23:33:31 +0800

    added password to sign up

--- teamup_mmu/features/user_signup/views.py
+++ teamup_mmu/features/user_signup/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-   return render(request, 'user_signup/templates/index.html', {'data':'data from signup endpoint'})
+   return render(request, 'user_signup/templates/index.html', {'data':'Test message from the views.py'})
 
 def receive(request):
    if request.method == 'POST':
       username = request.POST.get('username')
+      password = request.POST.get('password')
       # After the user is created/logged in successfully:
       response = HttpResponse()
       response["HX-Redirect"] = "/matching/" # This tells HTMX to redirect the WHOLE page

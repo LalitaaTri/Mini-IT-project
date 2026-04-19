d3755ea1b4e2795d44b394167dafec286b60985f
alimq <alimimanmalik2@gmail.com>
2026-04-19 19:22:39 +0800

    static image still not working

--- teamup_mmu/settings.py
+++ teamup_mmu/settings.py
@@ -119,6 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'

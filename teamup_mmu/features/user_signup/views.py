74f3000a64b07b72394696addbe20f4e476ceb2f
alimq <alimimanmalik2@gmail.com>
2026-04-19 12:31:11 +0800

    hashed password

--- teamup_mmu/features/user_signup/views.py
+++ teamup_mmu/features/user_signup/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from teamup_mmu.db import Database
+from django.contrib.auth.hashers import make_password
 
 async def receive(request):
     if request.method == 'POST':
       email = request.POST.get('email')
-      password = request.POST.get('password')
+      password = make_password(request.POST.get('password'))
       # After the user is created/logged in successfully:
       # response = HttpResponse()
       # response["HX-Redirect"] = "/matching/" # This tells HTMX to redirect the WHOLE page
@@ -20,4 +21,4 @@ async def index(request):
    pool = await Database.get_pool()
    async with pool.acquire() as conn:
       value = await conn.fetch("SELECT * FROM users")
-      return render(request, 'user_signup/templates/index.html', {'data':value})
+      return render(request, 'user_signup/templates/index.html')

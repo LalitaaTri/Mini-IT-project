2f61541e794025a10a3269e834c52325ad110712
Lalitaa <lalitaatripurasundari@gmail.com>
2026-05-18 02:16:46 +0800

    prevent class admins from leaving their own classes and implement class deletion logic

--- teamup_mmu/urls.py
+++ teamup_mmu/urls.py
@@ -24,6 +24,8 @@ from .features.user_classes.views import edit_class
 from .features.user_classes.views import remove_student 
 from .features.user_classes.views import share_code_modal
 from .features.user_classes.views import delete_class_modal
+from .features.user_classes.views import delete_class_confirm
+
 
 
 
@@ -104,6 +106,7 @@ urlpatterns = [
     path('classes/remove_student/<int:class_id>/<int:student_id>/', remove_student, name = 'remove_student'),
     path('classes/share_code_modal/<int:class_id>/', share_code_modal, name='share_code_modal'),
     path('classes/delete_class_modal/<int:class_id>/', delete_class_modal, name='delete_class_modal'),
+    path('classes/delete_class_confirm/', delete_class_confirm, name='delete_class_confirm'),
 
 
     path('admin/', admin.site.urls)

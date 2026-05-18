0b81e3c8f2b7c13cac86e91653b0b8948be774fd
Koh Yee Shaun <kyskyj0711@gmail.com>
2026-05-18 14:11:13 +0800

    small fixes

--- teamup_mmu/features/user_inbox/views.py
+++ teamup_mmu/features/user_inbox/views.py
@@ -1,4 +1,5 @@
 from ..user_access_check.views import *
+from django.shortcuts import render, redirect
 
 async def index(request):
     passed_login_check, status, email, id = await access_check(request)
